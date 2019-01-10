5e908c02a5c55560f1105e2102c9bd363d2d6c3b
Gilbert Fernandes <g_fernandes@mousquetaires.com>
2019-01-10 15:46:47 +0100

    add constructor to hide default one

--- decrypt_singlebyte_xor/src/decrypt_singlebyte_xor/Util.java
+++ decrypt_singlebyte_xor/src/decrypt_singlebyte_xor/Util.java
@@ -11,6 +11,10 @@ import java.util.List;
 
 public class Util {
 	
+	Util() {
+		// constructor
+	}
+	
 	// ---- checkArg ------------------------------------------------------------------------------
 
 	public static void checkArg(final String[] args, List<String> encodedStrings) {
